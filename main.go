f8d5268cd55af32745372daf99f3222ad1c1bba5
Eduardo Moraes(edu-bm7) <bm7.eduardo@gmail.com>
2023-03-12 19:40:47 -0300

    Added height, foreground and background color support

--- main.go
+++ main.go
@@ -12,10 +12,16 @@ import (
 
 var msg string
 var fdf string
+var bgColor string
+var fgColor string
+var height int64
 
 func main() {
-	flag.StringVar(&msg, "msg", "", "Message to be printed on screen")
-	flag.StringVar(&fdf, "map", "", "Name of the map file to be saved")
+	flag.StringVar(&msg, "msg", "", "Message to be printed on screen: E.g -msg Hello, World")
+	flag.StringVar(&fdf, "map", "", "Name of the map file to be saved: E.g. -map example.fdf")
+	flag.StringVar(&bgColor, "bg", "", "The background color in Hexadecimal: E.g. -bg 0xFFFFFF")
+	flag.StringVar(&fgColor, "fg", "", "The foreground color in Hexadecimal: E.g. -fg 0xFFFFFF")
+	flag.Int64Var(&height, "height", 3, "The height of the map(default value is 3): E.g. -height 5")
 	flag.Parse()
 
 	if msg == "" {
@@ -34,7 +40,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	font, err := figletlib.GetFontByName("figletlib/fonts/", "banner.flf")
+	font, err := figletlib.GetFontByName("figletlib/fonts", "banner.flf")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
@@ -59,28 +65,36 @@ func main() {
 	}(f)
 
 	// Write a string to the file
-	_, err = io.WriteString(f, textTofdf(text))
+	_, err = io.WriteString(f, textTofdf(text, bgColor, fgColor, height))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-func textTofdf(text string) string {
-	fdfstr := ""
+func textTofdf(text string, bgColor string, fgColor string, height int64) string {
+	fdfStr := ""
 	for _, line := range strings.Split(text, "\n") {
-		fdfline := ""
+		fdfLine := ""
 		for _, l := range line {
 			if l == ' ' {
-				fdfline += "0 0 0"
+				if bgColor != "" {
+					fdfLine += fmt.Sprintf("0,%s 0,%s 0,%s", bgColor, bgColor, bgColor)
+				} else {
+					fdfLine += "0 0 0"
+				}
 			} else if l == '#' {
-				fdfline += "3 3 3"
+				if fgColor != "" {
+					fdfLine += fmt.Sprintf("%d,%s %d,%s %d,%s", height, fgColor, height, fgColor, height, fgColor)
+				} else {
+					fdfLine += fmt.Sprintf("%d %d %d", height, height, height)
+				}
 			}
-			fdfline += " "
+			fdfLine += " "
 		}
-		fdfline += "\n"
-		fdfstr += fdfline + fdfline + fdfline
+		fdfLine += "\n"
+		fdfStr += fdfLine + fdfLine + fdfLine
 	}
-	fdfstr = strings.Trim(fdfstr, "\n")
-	return fdfstr
+	fdfStr = strings.Trim(fdfStr, "\n")
+	return fdfStr
 }
